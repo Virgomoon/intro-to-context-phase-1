fc577b4aad1e77bddce14dcfb7762f0e49021024
Joseph Guillory <jagmanx99@yahoo.com>
2022-05-17 20:27:12 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -45,7 +45,6 @@ function createTimeOutEvent(obj, dateTime) {
 }
 
 function hoursWorkedOnDate(obj, date) {
-    // console.log(obj)
     let timeIn = obj.timeInEvents.filter(el => (el.date === date))
     let timeOut = obj.timeOutEvents.filter(el =>(el.date === date)) 
 
@@ -63,11 +62,14 @@ function wagesEarnedOnDate(obj, date) {
 function allWagesFor(obj) {
 
     let dateArr = []
-    let dates = obj.timeInEvents.filter(el => el.date)
-    for( el of dates){
-        console.log(el.date)
+    console.log('test 1')
+    console.log(obj)
+    let dates = obj.timeInEvents.filter(el => {
+       
+        return el.date})
+    for(const el of dates){
+     
        dateArr.push(wagesEarnedOnDate(obj, el.date))
-       console.log(dateArr)
     }
     
     return dateArr.reduce((acc,next)=> acc + next)
@@ -76,84 +78,8 @@ function allWagesFor(obj) {
 
 function calculatePayroll(arr) {
     let employeeArr = []
-    for (obj of arr) {
+    for (const obj of arr) {
         employeeArr.push (allWagesFor(obj))
     }
     return employeeArr.reduce((acc,next)=> acc + next)
 }
-
-
-// const pracObj = createEmployeeRecord(['humpty', 'dumpty', 'rapper', 3])
-
-// cRecord = createEmployeeRecord(["Julius", "Caesar", "General", 27])
-// // Earns 324
-// updatedBpRecord = createTimeInEvent(cRecord, "0044-03-14 0900")
-// updatedBpRecord = createTimeOutEvent(cRecord, "0044-03-14 2100")
-// // Earns 54
-// updatedBpRecord = createTimeInEvent(cRecord, "0044-03-15 0900")
-// updatedBpRecord = createTimeOutEvent(cRecord, "0044-03-15 1100")
-// // 324 + 54
-// console.log(allWagesFor(cRecord))
-
-// const csvDataEmployees = [
-//     ["Thor", "Odinsson", "Electrical Engineer", 45],
-//     ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
-//     ["Natalia", "Romanov", "CEO", 150],
-//     ["Darcey", "Lewis", "Intern", 15],
-//     ["Jarvis", "Stark", "CIO", 125],
-//     ["Anthony", "Stark", "Angel Investor", 300]
-//   ]
-
-//   const csvTimesIn = [
-//     ["Thor", ["2018-01-01 0800", "2018-01-02 0800", "2018-01-03 0800"]],
-//     ["Loki", ["2018-01-01 0700", "2018-01-02 0700", "2018-01-03 0600"]],
-//     ["Natalia", ["2018-01-01 1700", "2018-01-02 1800", "2018-01-03 1300"]],
-//     ["Darcey", ["2018-01-01 0700", "2018-01-02 0800", "2018-01-03 0800"]],
-//     ["Jarvis", ["2018-01-01 0500", "2018-01-02 0500", "2018-01-03 0500"]],
-//     ["Anthony", ["2018-01-01 1400", "2018-01-02 1400", "2018-01-03 1400"]]
-//   ]
-
-//   const csvTimesOut = [
-//     ["Thor", ["2018-01-01 1600", "2018-01-02 1800", "2018-01-03 1800"]],
-//     ["Loki", ["2018-01-01 1700", "2018-01-02 1800", "2018-01-03 1800"]],
-//     ["Natalia", ["2018-01-01 2300", "2018-01-02 2300", "2018-01-03 2300"]],
-//     ["Darcey", ["2018-01-01 1300", "2018-01-02 1300", "2018-01-03 1300"]],
-//     ["Jarvis", ["2018-01-01 1800", "2018-01-02 1800", "2018-01-03 1800"]],
-//     ["Anthony", ["2018-01-01 1600", "2018-01-02 1600", "2018-01-03 1600"]]
-//   ]
-
-//   let employeeRecords = createEmployeeRecords(csvDataEmployees)
-//             employeeRecords.forEach(function (rec) {
-//               let timesInRecordRow = csvTimesIn.find(function (row) {
-//                 return rec.firstName === row[0]
-//               })
-
-//               let timesOutRecordRow = csvTimesOut.find(function (row) {
-//                 return rec.firstName === row[0]
-//               })
-
-//               timesInRecordRow[1].forEach(function(timeInStamp){
-//                 createTimeInEvent(rec, timeInStamp)
-//               })
-
-//               timesOutRecordRow[1].forEach(function(timeOutStamp){
-//                 createTimeOutEvent(rec, timeOutStamp)
-//               })
-
-//             })
-
-//             console.log(calculatePayroll(employeeRecords))
-
-
-// console.log(createEmployeeRecord('gray', 'worm', 'security', 1))
-// console.log(createEmployeeRecord(['humpty', 'dumpty', 'rapper', 3]))
-// console.log(createTimeInEvent(pracObj, '2022-05-22 0800'))
-// console.log(createTimeOutEvent(pracObj, '2022-05-22 1600'))
-// console.log(createTimeInEvent(pracObj, '2022-06-22 0800'))
-// console.log(createTimeOutEvent(pracObj, '2022-06-22 1600'))
-// console.log('hours wroked')
-// console.log(hoursWorkedOnDate(pracObj,'2022-05-22'))
-// console.log(wagesEarnedOnDate(pracObj, '2022-05-22'))
-// console.log(pracObj.timeInEvents)
-// console.log('all wages')
-// console.log(allWagesFor(pracObj))
